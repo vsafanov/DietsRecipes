0da2556bbaa36742fc1321d07f38af6d49bcc291
Valeriy Safanov <vsafanov@hotmail.com>
2024-04-11 01:02:49 -0400

    updated v16, commented bootstrap ref

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule,APP_INITIALIZER } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
@@ -8,27 +8,28 @@ import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RecipesListComponent } from "./recipes-list.component";
 import { RecipeDetailsComponent } from "./recipe-details.component";
-import {ConfigService} from "./config.service";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { ConfigService } from "./config.service";
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        //Ng2Bs3ModalModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        HttpClientJsonpModule,
-        NgbModule],
-    declarations: [
-        AppComponent,
-        RecipesListComponent,
-        RecipeDetailsComponent],
-    bootstrap: [AppComponent],
-    providers: [ConfigService,
-      {
+  imports: [
+    BrowserModule,
+    FormsModule,
+    //Ng2Bs3ModalModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    // NgbModule
+  ],
+  declarations: [
+    AppComponent,
+    RecipesListComponent,
+    RecipeDetailsComponent],
+  bootstrap: [AppComponent],
+  providers: [ConfigService,
+    {
       provide: APP_INITIALIZER,
-      useFactory: (config:ConfigService) => () => config.load(),
+      useFactory: (config: ConfigService) => () => config.load(),
       deps: [ConfigService],
       multi: true
     }
